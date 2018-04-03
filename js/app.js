bc1c21166a56f26fe251dd6b0479119491b084b2
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-03 16:33:14 -0700

    question 1 functionality complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 'use strict';
 
-alert('It is working');
+// question #1
+
+var question = 'Am I a native Seattlite?';
+var userAnswer = prompt(question).toLowerCase();
+var response = '';
+console.log('Question: ', question);
+console.log('>>>User answer: ', userAnswer);
+
+if (userAnswer === 'n') {
+  response = 'That is correct!';
+  console.log('Answer status:');
+} else if (userAnswer === 'no') {
+  response = 'That is correct!';
+  console.log('Answer status:');
+} else {
+  response = 'Sorry, that is incorrect.';
+  console.log('>>>Answer status: Incorrect!');
+}
+response += '\nLike 93.6% of the people that live in Seattle, I grew up in California!';
+alert(response);
+// question #2
+
+
+// question #3
+
+
+// question #4
+
+
+// question #5
+
