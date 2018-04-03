03a23673d47fb655e086171b4e474110f748c821
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-03 16:56:59 -0700

    all 5 questions js functionality complete

--- js/app.js
+++ js/app.js
@@ -1,33 +1,98 @@
 'use strict';
+var question;
+var userAnswer;
+var response;
 
 // question #1
-
-var question = 'Am I a native Seattlite?';
-var userAnswer = prompt(question).toLowerCase();
-var response = '';
+question = 'Am I a native Seattlite?';
+userAnswer = prompt(question).toLowerCase();
 console.log('Question: ', question);
-console.log('>>>User answer: ', userAnswer);
+console.log('...User answer: ', userAnswer);
 
 if (userAnswer === 'n') {
   response = 'That is correct!';
-  console.log('Answer status:');
+  console.log('...Answer status: Correct');
 } else if (userAnswer === 'no') {
   response = 'That is correct!';
-  console.log('Answer status:');
+  console.log('...Answer status: Correct');
 } else {
   response = 'Sorry, that is incorrect.';
-  console.log('>>>Answer status: Incorrect!');
+  console.log('...Answer status: Incorrect!');
 }
-response += '\nLike 93.6% of the people that live in Seattle, I grew up in California!';
-alert(response);
-// question #2
 
+alert(response);
+
+// question #2
+question = 'Have I successfully ridden my bicycle all the way from San Fransisco to Virginia?';
+userAnswer = prompt(question).toLowerCase();
+console.log('Question: ', question);
+console.log('...User answer: ', userAnswer);
+
+if (userAnswer === 'n') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else if (userAnswer === 'no') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else {
+  response = 'Sorry, that is incorrect.';
+  console.log('...Answer status: Incorrect!');
+}
+alert(response);
 
 // question #3
+question = 'Do I have a wife?';
+userAnswer = prompt(question).toLowerCase();
+console.log('Question: ', question);
+console.log('...User answer: ', userAnswer);
 
+if (userAnswer === 'y') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else if (userAnswer === 'yes') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else {
+  response = 'Sorry, that is incorrect.';
+  console.log('...Answer status: Incorrect!');
+}
+
+alert(response);
 
 // question #4
+question = 'Do I have any pets?';
+userAnswer = prompt(question).toLowerCase();
+console.log('Question: ', question);
+console.log('...User answer: ', userAnswer);
 
+if (userAnswer === 'n') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else if (userAnswer === 'no') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else {
+  response = 'Sorry, that is incorrect.';
+  console.log('...Answer status: Incorrect!');
+}
+
+alert(response);
 
 // question #5
+question = 'Do I like sports?';
+userAnswer = prompt(question).toLowerCase();
+console.log('Question: ', question);
+console.log('...User answer: ', userAnswer);
 
+if (userAnswer === 'n') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else if (userAnswer === 'no') {
+  response = 'That is correct!';
+  console.log('...Answer status: Correct');
+} else {
+  response = 'Sorry, that is incorrect.';
+  console.log('...Answer status: Incorrect!');
+}
+
+alert(response);
